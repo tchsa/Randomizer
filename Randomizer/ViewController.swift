dcd41e58e6e3151d7f8797b9caae54e2c337ba20
MSP <mit_sergey@mail.ru>
2024-12-22 12:00:54 +0700

    Init project

--- Randomizer/ViewController.swift
+++ Randomizer/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  Randomizer
-//
-//  Created by Сергей Митев on 22.12.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
    override func viewDidLoad() {
       super.viewDidLoad()
-      // Do any additional setup after loading the view.
+      
    }
 
 
